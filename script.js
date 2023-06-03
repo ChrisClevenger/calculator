6e7a1bd0d7553638105891d60427da71cd348a98
ChrisClevenger <aenguscoen@gmail.com>
2023-06-03 13:11:18 -0500

    Add html/js for operand buttons

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const integers = document.querySelectorAll('.number'); 
+const operationButtons = document.querySelectorAll('.operand'); 
+const equalsButton = document.querySelector('#equalsButton'); 
+const clearButton = document.querySelector('#clearButton'); 
+const deleteButton = document.querySelector('#deleteButton'); 
+
 //Array holder for button logs
 
 const i = [];  
@@ -5,13 +11,22 @@ const i = [];
 // Function to take button presses and convert to string (of integers)
 
 document.addEventListener('DOMContentLoaded', function() {
-    var integers = document.querySelectorAll('.number');
     integers.forEach(function(integer) {
       integer.addEventListener('click', function(e) {
         i.push(this.value);
+
+        // Convert array to string
+   
     let integerString = i.toString(); 
+
+    // Remove commas
+
     integerString = integerString.replaceAll(',', ''); 
+
+    //Log for new integerString
+
     console.log(integerString); 
       });
     });
   });
+
